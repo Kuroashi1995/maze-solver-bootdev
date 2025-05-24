34a27dc02e01090caa00e506398000b0a1c72b1f
Kuroashi1995 <kuroashi.prog@gmail.com>
2025-05-24 16:55:08 -0300

    add draw move method to cell

--- graphics.py
+++ graphics.py
@@ -69,3 +69,19 @@ class Cell:
             self.__win.draw_line(Line(Point(self.__x1, self.__y1), Point(self.__x2, self.__y1)),"red")
         if self.has_bottom_wall:
             self.__win.draw_line(Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2)),"red")
+
+    def draw_move(self, to_cell, undo=False):
+        if not undo:
+            color = "red"
+        else:
+            color = "gray"
+        line = Line(
+            Point(
+                (self.__x1 + self.__x2)//2,
+                (self.__y1 + self.__y2)//2),
+            Point(
+                (to_cell.__x1 + to_cell.__x2)//2,
+                (to_cell.__y1 + to_cell.__y2)//2,
+            )
+        )
+        self.__win.draw_line(line, color)
