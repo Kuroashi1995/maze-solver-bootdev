0857ac9243470099e6f3f225d7b35f33879af68c
Kuroashi1995 <kuroashi.prog@gmail.com>
2025-05-24 04:50:47 -0300

    Add Cell class

--- graphics.py
+++ graphics.py
@@ -44,3 +44,28 @@ class Window:
     def draw_line(self, line: Line, fill_color: str):
         line.draw(self.__canvas, fill_color)
 
+class Cell:
+    def __init__(self, window: Window) -> None:
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self.__x1 = -1
+        self.__y1 = -1
+        self.__x2 = -1
+        self.__y2 = -1
+        self.__win = window
+
+    def draw(self, x1: int, y1: int, x2: int, y2: int):
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__x2 = x2
+        self.__y2 = y2
+        if self.has_left_wall:
+            self.__win.draw_line(Line(Point(self.__x1, self.__y1), Point(self.__x1, self.__y2)),"red")
+        if self.has_right_wall:
+            self.__win.draw_line(Line(Point(self.__x2, self.__y1), Point(self.__x2, self.__y2)),"red")
+        if self.has_top_wall:
+            self.__win.draw_line(Line(Point(self.__x1, self.__y1), Point(self.__x2, self.__y1)),"red")
+        if self.has_bottom_wall:
+            self.__win.draw_line(Line(Point(self.__x1, self.__y2), Point(self.__x2, self.__y2)),"red")
