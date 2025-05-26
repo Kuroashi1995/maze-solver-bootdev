7d50172bd02ff1936256a18168b315b8829e2c0e
Kuroashi1995 <kuroashi.prog@gmail.com>
2025-05-26 18:24:10 -0300

    render maze

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ def main():
     win = Window(win_width, win_height)
     padding_x = 50
     padding_y = 50
-    cells_width = 40
-    cells_height = 40
+    cells_width = 20
+    cells_height = 20
     cols = (win_width - 2*padding_x) // cells_width
     rows = (win_height - 2*padding_y) // cells_height
     maze = Maze(padding_x, padding_y, rows, cols, cells_width, cells_height, win)
+    maze.solve()
     win.wait_for_close()
 
 main()
