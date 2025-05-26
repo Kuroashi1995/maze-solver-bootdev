300636c7ea24db61b8944bf2fffaa5ee1e242559
Kuroashi1995 <kuroashi.prog@gmail.com>
2025-05-26 01:27:29 -0300

    render maze

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from graphics import Cell, Window, Line, Point, Maze
 
 def main():
-    win = Window(800, 600)
-    maze = Maze(0, 0, 20, 20, 50, 50, win)
+    win_width = 800
+    win_height = 600
+    win = Window(win_width, win_height)
+    padding_x = 50
+    padding_y = 50
+    cells_width = 40
+    cells_height = 40
+    cols = (win_width - 2*padding_x) // cells_width
+    rows = (win_height - 2*padding_y) // cells_height
+    maze = Maze(padding_x, padding_y, rows, cols, cells_width, cells_height, win)
     win.wait_for_close()
 
 main()
